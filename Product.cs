ce0ca10f9a1840346dc1090b968e44b4eab49905
Veli Keçeci <33519773+vlikcc@users.noreply.github.com>
2022-03-25 14:24:07 +0300

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product : IEntity
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int ProductImageId { get; set; }
+        public int RecipeId { get; set; }
+        public int DiscountId { get; set; }
+        public int UnitId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+    }
+}
